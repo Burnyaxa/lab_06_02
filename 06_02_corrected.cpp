d01c2d7ceaddeb5ba4b644233fd24631c4f2bef4
Artem Supryhan <42550216+Burnyaxa@users.noreply.github.com>
2018-11-25 02:03:46 +0200

    Update 06_02_corrected.cpp
    Исправлены стилистические ошибки. @nechaido

--- 06_02_corrected.cpp
+++ 06_02_corrected.cpp
@@ -1,44 +1,48 @@
-/*2.Try trykutnyky zadani svoimy storonamy A, B ta C. 
+/*2.Try trykutnyky zadani svoimy storonamy A, V ta S. 
 Za dopomohoiu formuly Herona obchyslyty ploshchi trykutnykiv ta vyznachyty,
 ploshcha yakoho z nykh ye naibilshoiu.
 */
-
 #include "stdafx.h"
 #include <iostream>
 #include <math.h>
 using namespace std;
-double arr_square[3];
-void input(int n);
-double square(double A,double B,double C);
-double max_square();
+
+void input(int n, double arrArea[3]);
+double getArea(double A,double B,double C);
+double getMaxArea(double arrArea[3]);
+
 int _tmain(int argc, _TCHAR* argv[]){
+	double arrArea[3];
 	for (int n = 1; n <= 3; n++){
-		input(n);
+		input(n, arrArea);
 	}
-	cout << "The biggest square - " << max_square()<<endl;
+	cout << "The biggest area - " << getMaxArea(arrArea)<<endl;
 	system("pause");
 	return 0;
 }
-void input(int n){
+
+void input(int n, double arrArea[3]){
 	double A,B,C,S;
-	cout <<"A"<<n<< " ="; cin >> A;
-	cout <<"B"<<n<< " ="; cin >> B;
-	cout <<"C"<<n<< " ="; cin >> C;
-	S = square(A, B, C);
-	cout <<"S" <<n<<" ="<<S<<endl;
-	arr_square[n - 1] =S;
+	cout << "A" << n << " ="; cin >> A;
+	cout << "B" << n << " ="; cin >> B;
+	cout << "C" << n << " ="; cin >> C;
+	S = getArea(A, B, C);
+	cout << "S" << n << " =" << S <<endl;
+	arrArea[n - 1] =S;
 }
-double square(double A, double B, double C){
+
+double getArea(double A, double B, double C){
 	double P = (A + B + C) / 2.;
 	double S = sqrt(P*(P - A)*(P - B)*(P - C));
 	return S;
 }
-double max_square(){
-	double max_square = arr_square[0];
+
+double getMaxArea(double arrArea[3]){
+	double maxArea = arrArea[0];
 	for (int i = 1; i < 3; i++){
-		if (arr_square[i] > max_square){
-			max_square = arr_square[i];
+		if (arrArea[i] > maxArea){
+			maxArea = arrArea[i];
 		}
 	}
-	return max_square;
+	return maxArea;
 }
